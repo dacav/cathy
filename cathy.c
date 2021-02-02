9e8c0b9de5aa1c172dfd3b0215f0f1ca5cbba563
Dacav <dacav@fastmail.com>
2021-02-02 23:26:34 +0100

    OutDir

--- cathy.c
+++ cathy.c
@@ -2,10 +2,12 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <limits.h>
 
 /* -- IORead ---------------------------------------------------------- */
 
@@ -50,22 +52,95 @@ const char *IORead_next(IORead *ioread)
     return ioread->buffer;
 }
 
-/* -- misc ------------------------------------------------------------ */
+/* -- OutDir ---------------------------------------------------------- */
 
-const char * file_hash(const char *filename, char *buffer, size_t buflen)
+enum {
+    OutDir_PREFIX = 3,
+};
+
+typedef struct {
+    char *buffer;
+    size_t base_path_len;
+    size_t hashlen;
+} OutDir;
+
+void OutDir_free(OutDir *outdir)
+{
+    free(outdir->buffer);
+}
+
+int OutDir_init(OutDir *outdir, const char *path, size_t hashlen)
+{
+    char *rpath;
+
+    *outdir = (OutDir){
+        .hashlen = hashlen,
+    };
+
+    rpath = realpath(path, NULL);
+    if (!rpath) {
+        warn("realpath(%s, NULL)", path);
+        goto fail;
+    }
+
+    outdir->base_path_len = strlen(rpath);
+
+    /* outdir + '/' + hash[0:N] + '/' + hash[N:] + '\0' */
+    size_t len = outdir->base_path_len + 3 + hashlen;
+
+    outdir->buffer = realloc(rpath, len);
+    if (!outdir->buffer)
+        goto fail;
+    rpath = NULL;
+
+    outdir->buffer[outdir->base_path_len++] = '/';
+    outdir->buffer[outdir->base_path_len] = '\0';
+
+    return 0;
+
+fail:
+    free(rpath);
+    OutDir_free(outdir);
+    return -1;
+}
+
+const char * OutDir_path(OutDir *outdir, const char *hash)
+{
+    const size_t hashlen = outdir->hashlen;
+    char *buffer = outdir->buffer;
+    int w = outdir->base_path_len;
+
+    for (int i = 0; i < hashlen && hash[i]; ++i) {
+        if (i == OutDir_PREFIX)
+            buffer[w++] = '/';
+        buffer[w++] = hash[i];
+    }
+
+    buffer[w++] = '\0';
+
+    return outdir->buffer;
+}
+
+/* -- Hash ------------------------------------------------------------ */
+
+enum {
+    Hash_checksum_length = 32,
+    Hash_buflen = Hash_checksum_length + 1,
+};
+
+const char * Hash_file(const char *filename, char *buffer, size_t buflen)
 {
     enum {
         r = 0,
         w = 1,
-        min_buflen = 33,
     };
 
     pid_t pid = -1;
     ssize_t nread;
     int pipefd[2] = {-1, -1};
 
-    if (buflen < min_buflen) {
-        warnx("min buflen is %d - %zu is not enough", min_buflen, buflen);
+    if (buflen < Hash_buflen) {
+        warnx("min buflen is %d - %zu is not enough", Hash_buflen, buflen);
         goto fail;
     }
 
@@ -139,27 +214,38 @@ fail:
 int main(int argc, char **argv)
 {
     IORead ioread;
+    OutDir outdir;
     int ex = EXIT_FAILURE;
 
     const char *fname;
 
+    const char *outdir_path = "./cathy"; // TODO: argv
+
     IORead_init(&ioread);
 
+    if (OutDir_init(&outdir, outdir_path, Hash_checksum_length))
+        goto exit;
+
     while (fname = IORead_next(&ioread), fname != NULL) {
-        char buffer[33];
+        char buffer[Hash_buflen];
         const char *hash;
 
-        hash = file_hash(fname, buffer, sizeof(buffer));
+        hash = Hash_file(fname, buffer, sizeof(buffer));
         if (!hash) {
             warnx("skipping %s: could not compute hash", fname);
             continue;  // TODO - count failure?
         }
 
+        printf("fname=%s\n hash=%s\n path=%s\n",
+            fname, hash, OutDir_path(&outdir, hash)
+        );
     }
 
     if (ioread.errno_save == 0)
         ex = EXIT_SUCCESS;
 
+exit:
+    OutDir_free(&outdir);
     IORead_free(&ioread);
     return ex;
 }
