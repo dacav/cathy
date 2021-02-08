04e2f5a295959b4786174c249ab13ad1dbbfad60
Dacav <dacav@fastmail.com>
2021-02-08 23:55:28 +0100

    clean now useless includes

--- cathy.c
+++ cathy.c
@@ -1,16 +1,5 @@
-#include <dirent.h>
 #include <err.h>
-#include <errno.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <limits.h>
 
 #include "hash.h"
 #include "ioread.h"
