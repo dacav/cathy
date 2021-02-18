e4cecf3a4b00099ada5da592744d61b932e8ecd4
Dacav <dacav@fastmail.com>
2021-02-18 22:38:20 +0100

    Avoid a file listed twice to be unlinked
    File_identical detects duplicates at inode level

--- file.h
+++ file.h
@@ -1,13 +1,18 @@
 #pragma once
 
+#include <stdbool.h>
+#include <sys/stat.h>
 #include <time.h>
 
 typedef struct File {
     const char *path;
     time_t mtime;
+    ino_t inode_id;
 } File;
 
 int File_init(File *, const char *path);
+
+bool File_identical(File *, File *);
 
 void File_objswap(File *, File *);
 
